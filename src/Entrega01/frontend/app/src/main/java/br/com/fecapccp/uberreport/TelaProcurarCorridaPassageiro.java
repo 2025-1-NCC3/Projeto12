685cd7989abb186b12e7d94e4de4610783e87a68
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-03-23 20:04:31 -0300

    Testes de alertas efetuados - Close #23

--- src/Entrega01/frontend/app/src/main/java/br/com/fecapccp/uberreport/TelaProcurarCorridaPassageiro.java
+++ src/Entrega01/frontend/app/src/main/java/br/com/fecapccp/uberreport/TelaProcurarCorridaPassageiro.java
@@ -78,7 +78,6 @@ public class TelaProcurarCorridaPassageiro extends AppCompatActivity implements 
     private double userLatitude;
     private double userLongitude;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
