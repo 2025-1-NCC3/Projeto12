79856cbe65fcc05e1067954f3b77f0b263d1cd23
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-03-31 20:22:47 -0300

    Atualizando link do servidor

--- src/Entrega01/frontend/app/src/main/java/br/com/fecapccp/uberreport/logicas/requisicoes/ServidorApi.java
+++ src/Entrega01/frontend/app/src/main/java/br/com/fecapccp/uberreport/logicas/requisicoes/ServidorApi.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServidorApi {
     private static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://lwjy86-3000.csb.app")
+            .baseUrl("https://xmzvrk-3000.csb.app")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
